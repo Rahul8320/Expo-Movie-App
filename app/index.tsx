ff7f92d67493615b64f0ffa4c10902010826b058
Rahul Dey <rahuldey832000@gmail.com>
2025-03-14 16:57:37 +0530

    Add custom color schema for tailwind css

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome to the Movie App</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl text-dark-200 font-bold">
+        Welcome to the Movie App
+      </Text>
     </View>
   );
 }
