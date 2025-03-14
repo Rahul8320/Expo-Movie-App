43d0f810ee21a3bf0ec2240fdec0580d49790727
Rahul Dey <rahuldey832000@gmail.com>
2025-03-14 17:21:21 +0530

    Add base tab and stack navigation setup

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+// TODO: rename it to bookmarks
+export default function Saved() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl text-dark-200 font-bold">Saved</Text>
+    </View>
+  );
+}
