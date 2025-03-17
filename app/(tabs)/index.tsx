ec352216e5a05e29b88db85040d02782bd65036c
Rahul Dey <rahuldey832000@gmail.com>
2025-03-17 08:17:04 +0530

    Add show latest movies in home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,26 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import { useFetch } from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import { View, Image, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }), true);
 
   return (
     <View className="flex-1 bg-primary">
@@ -18,13 +33,48 @@ export default function Index() {
       >
         <Image source={icons.logo} className="w-12 h-10 mt-12 mb-5 mx-auto" />
 
-        <View className="flex-1 mt-5">
-          <SearchBar
-            placeholder="Search for a movie"
-            inputMode="none"
-            onPress={() => router.push("/search")}
+        {moviesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
           />
-        </View>
+        ) : moviesError ? (
+          <Text className="text-white text-center mt-10">
+            {moviesError?.message}
+          </Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              placeholder="Search for a movie"
+              inputMode="none"
+              onPress={() => router.push("/search")}
+            />
+
+            <>
+              <Text className="text-white text-lg font-bold mt-5 mb-3">
+                Latest Movies
+              </Text>
+
+              <FlatList
+                data={movies}
+                renderItem={({ item }) => (
+                  <Text className="text-white text-sm">{item.title}</Text>
+                )}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: "flex-start",
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10,
+                }}
+                className="mt-2 pb-32"
+                scrollEnabled={false}
+              />
+            </>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
