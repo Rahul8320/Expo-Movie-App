b0f654dff4fb17a75dac842daf771b79ee810dc4
Rahul Dey <rahuldey832000@gmail.com>
2025-03-21 11:02:49 +0530

    Update save screen

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,13 @@
-import { View, Text } from "react-native";
+import { icons } from "@/constants/icons";
+import { View, Text, Image } from "react-native";
 
-// TODO: rename it to bookmarks
 export default function Saved() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl text-dark-200 font-bold">Saved</Text>
+    <View className="flex-1 bg-primary px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.save} className="size-10" tintColor="#fff" />
+        <Text className="text-base text-gray-500">Saved</Text>
+      </View>
     </View>
   );
 }
