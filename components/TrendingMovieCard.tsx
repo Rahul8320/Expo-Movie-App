391a99b50247594daaf59bc67aa29e1948c1eb95
Rahul Dey <rahuldey832000@gmail.com>
2025-03-21 08:21:00 +0530

    Style trending movie card

--- components/TrendingMovieCard.tsx
+++ components/TrendingMovieCard.tsx
@@ -1,5 +1,7 @@
 import { Link } from "expo-router";
 import { View, Text, TouchableOpacity, Image } from "react-native";
+import MaskedView from "@react-native-masked-view/masked-view";
+import { images } from "@/constants/images";
 
 export default function TrendingMovieCard({
   trendingMovie: { movie_id, poster_url, title },
@@ -13,6 +15,27 @@ export default function TrendingMovieCard({
           className="w-32 h-48 rounded-lg"
           resizeMode="cover"
         />
+
+        <View className="absolute bottom-3 -left-1.5 px-2 py-1 rounded-full">
+          <MaskedView
+            maskElement={
+              <Text className="text-5xl font-bold text-white">{index + 1}</Text>
+            }
+          >
+            <Image
+              source={images.rankingGradient}
+              className="size-14"
+              resizeMode="cover"
+            />
+          </MaskedView>
+        </View>
+
+        <Text
+          className="text-sm font-bold text-light-200 mt-2"
+          numberOfLines={2}
+        >
+          {title}
+        </Text>
       </TouchableOpacity>
     </Link>
   );
