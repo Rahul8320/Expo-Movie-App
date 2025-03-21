84e07cf20989b34ed23c37161ca92aa2ece52fed
Rahul Dey <rahuldey832000@gmail.com>
2025-03-21 10:59:43 +0530

    Update profile screen

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,13 @@
-import { View, Text } from "react-native";
+import { icons } from "@/constants/icons";
+import { View, Text, Image } from "react-native";
 
 export default function Profile() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl text-dark-200 font-bold">Profile</Text>
+    <View className="flex-1 bg-primary px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.person} className="size-10" tintColor="#fff" />
+        <Text className="text-base text-gray-500">Profile</Text>
+      </View>
     </View>
   );
 }
